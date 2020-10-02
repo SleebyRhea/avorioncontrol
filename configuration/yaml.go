601ffb656a05f0f7ea7e6bb41260eae87f20c9b5
Andrew Wyatt <sleepyfugu@gmail.com>
2020-10-02 16:51:02 +0000

    Change formatting a bit

--- configuration/yaml.go
+++ configuration/yaml.go
@@ -1,8 +1,8 @@
 package configuration
 
 type yamlDataCore struct {
-	TimeZone string `yaml:"time_zone"`
 	LogLevel int    `yaml:"log_level"`
+	TimeZone string `yaml:"time_zone"`
 	LogDir   string `yaml:"log_directory"`
 	DBName   string `yaml:"db_filename"`
 }
@@ -11,16 +11,17 @@ type yamlDataGame struct {
 	GalaxyName string `yaml:"galaxy_name"`
 	InstallDir string `yaml:"install_dir"`
 	DataDir    string `yaml:"data_dir"`
-	GamePort   int    `yaml:"port"`
 	PingPort   int    `yaml:"ping_port"`
+	GamePort   int    `yaml:"port"`
 }
 
 type yamlDataDiscord struct {
-	ChatChannel      string              `yaml:"channel"`
-	BotsAllowed      bool                `yaml:"bots_allowed"`
-	DiscordLink      string              `yaml:"invite"`
-	Prefix           string              `yaml:"prefix"`
-	Token            string              `yaml:"token"`
+	ChatChannel string `yaml:"channel"`
+	BotsAllowed bool   `yaml:"bots_allowed"`
+	DiscordLink string `yaml:"invite"`
+	Prefix      string `yaml:"prefix"`
+	Token       string `yaml:"token"`
+
 	AliasedCommands  map[string][]string `yaml:"aliased_commands"`
 	DisabledCommands []string            `yaml:"disabled_commands,flow"`
 }
