d5858734247c74f16d5a91bdd9866f7df957ccdc
Andrew Wyatt <sleepyfugu@gmail.com>
2020-10-02 21:57:30 +0000

    rules command now uses command library

--- mods/avocontrol-utilities/data/scripts/commands/rules.lua
+++ mods/avocontrol-utilities/data/scripts/commands/rules.lua
@@ -1,83 +1,93 @@
 --[[
-  AvorionControl Utilities
-  --------------------------
-  Whenever invoked by a player, will read from the rules file and
-  output the text found therein. Each newline corresponds to a *new* rule, so
-  lines can be as long as you like.
 
+  AvorionControl - data/scripts/commands/rules.lua
+  -----------------------------
+
+  When invoked, this command will read from a file named "RulesList.txt"
+  placed within the galaxy root (the output of Server().folder). The
+  syntax said file is as follows:
+
+  ";": A comment. Lines beginning with this character are ignored
+
+  "#": Disable formatting. Lines beginning with this character will
+       simply be output as is.
+
+  "-" or "+": These lines are formatted as a bullet point for a rule
+
+  All other non-empty lines present are treated as new rules and are
+  output with numbered formatting. Here is an source, and its output:
+
+  Note: Leading and trailing whitespaces are trimmed.
+
+  Example Source:
+    # =|Server Rules|=
+    # NOTE: Simple note
+    ; This is completely ignored
+    Rule about something
+      - More information on that rule
+    Rule about something else
+      + Another bit of info, but on this rule instead
+
+  Example Output:
+    =|Server Rules|=
+    NOTE: Simple note
+    1. Rule about something
+      - More information on that rule
+    2. Rule about something else
+      - Another bit of info, but on this rule instead
+  
+  
   License: BSD-3-Clause
   https://opensource.org/licenses/BSD-3-Clause
+
 ]]
 
-do
-  local __file  = Server().folder .. "/RulesList.txt"
-  local __name  = 'rules'
-  local __desc  = "Output the server rules"
-  local __usage = ''
-  local __old_path = package.path
-  
-  package.path = package.path .. ";data/scripts/lib/?.lua"
-  include("utility")
-  include("stringutility")
-  include("weapontype")
+package.path = package.path .. ";data/scripts/lib/?.lua"
+include("stringutility")
+include("avocontrol-utils")
 
-  local function __does_file_exist(name)
-    local f=io.open(name,"r")
-    if f~=nil then io.close(f) return true else return false end
+local command       = include("avocontrol-command")
+command.name        = "rules"
+command.description = "Output the server rules"
+
+command:SetExecute(function(sender)
+  local file = Server().folder .. "/RulesList.txt"
+  local resp = ""
+  
+  if not FileExists(file) then
+    return 1, "", "No rules defined!"
+  end
+  
+  local c = 0
+  for l in io.lines(file) do
+    local parsed = false
+    
+    l = string.gsub(l, "^%s*(.-)%s*$", "%1")
+    
+    if type(l) == "nil" or l == "" or string.sub(l,1,1) == ";" then
+      goto continue
+      
+    elseif string.sub(l,1,1) == "#" then
+      l = string.gsub(l, "^%s*#*%s*(.-)%s*$", "%1")
+      parsed = "${r}\n"%_T % {c=c,r=l}
+      
+    elseif string.sub(l,1,1) == "-" or string.sub(l,1,1) == "+" then
+      l = string.gsub(string.sub(l,2,-1), "^%s*[-+]*%s*(.-)%s*$", "%1")
+      parsed = "    - ${r}\n"%_T % {c=c, r=l}
+      
+    else
+      c = c + 1
+      parsed = "${c}: ${r}\n"%_T % {c=c, r=l}
+    end
+    
+    ::continue::    
+    resp = (parsed and resp..parsed or resp)
+  end
+  
+  if type(sender) ~= "nil" then
+    print("Player <${p}> has read the server rules"%_T % {
+      p=Player(sender).name})
   end
 
-  function execute(sender, commandName, modName, ...)
-    if type(sender) ~= "nil" then
-      print("Player <${p}> has read the server rules"%_T % {
-        p=Player(sender).name
-      })
-    end
-    
-    local args = {...}
-    local response = ""
-    
-    if not __does_file_exist(__file) then
-      return 1, "", "No rules defined!"
-    end
-    
-    local c = 0
-    for l in io.lines(__file) do
-      local __resp = false
-      
-      l = string.gsub(l, "^%s*(.-)%s*$", "%1")
-      
-      if type(l) == "nil" or l == "" or string.sub(l,1,1) == ";" then
-        -- Stubbed. Lua has no continue statement so this has to
-        -- remain. It makes me sad inside :/
-        -- Anyway, this catches empty lines, lines filled with
-        -- whitespace that were blanked out, and comments.
-        
-      elseif string.sub(l,1,1) == "#" then
-        l = string.gsub(l, "^%s*#*%s*(.-)%s*$", "%1")
-        __resp = "${r}\n"%_T % {c=c,r=l}
-        
-      elseif string.sub(l,1,1) == "-" or string.sub(l,1,1) == "+" then
-        l = string.gsub(string.sub(l,2,-1), "^%s*[-+]*%s*(.-)%s*$", "%1")
-        __resp = "    - ${r}\n"%_T % {c=c, r=l}
-        
-      else
-        c = c + 1
-        __resp = "${c}: ${r}\n"%_T % {c=c, r=l}
-      end
-      
-      response = (__resp and response .. __resp or response)
-    end
-    
-    return 0, response, ""
-  end
-  
-  function getDescription()
-    return __desc.."."
-  end
-  
-  function getHelp()
-    return "${d}. Usage: /${n} ${u}"%_T % {d=__desc, n=__name, u=__usage}
-  end
-  
-  package.path = __old_path
-end
+  return 0, resp, ""
+end)
