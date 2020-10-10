3a7adf5eeeeee1862f32eef8e99cdd26253598e9
Andrew Wyatt <sleepyfugu@gmail.com>
2020-10-10 22:41:51 +0000

    Refactor everything

--- mods/avocontrol-utilities/data/scripts/sector/background/duelzone.lua
+++ mods/avocontrol-utilities/data/scripts/sector/background/duelzone.lua
@@ -22,13 +22,9 @@ include ("stringutility")
 
 -- namespace DuelZoneSector
 DuelZoneSector = {}
-if not onServer() then
-  return
-end
 
-local isEternal   = false
-local playersHere = false
-
+DuelZoneSector.isEternal = false
+DuelZoneSector.isEnabled = false
 
 -- stripZone strips a zone from the list of zones
 local function stripZone(zones, zone)
@@ -39,6 +35,40 @@ local function stripZone(zones, zone)
   return zones
 end
 
+local function pvpon()
+  local s = Sector()
+  local x, y   = s:getCoordinates()
+  local zone   = x.."_"..y
+  local zones = (Server():getValue("duelzones") or "") 
+  s.pvpDamage = true
+  s:setValue("duelzone", true)
+  DuelZoneSector.isEnabled = true
+  print(zone..": PvP enabled")
+  Server():setValue("duelzones", 
+    string.gsub(stripZone(zones, zone) .. ":" .. zone, "^:*(.-):*$", "%1"))
+end
+
+local function pvpoff()
+  local s = Sector()
+  local x, y   = s:getCoordinates()
+  local zone   = x.."_"..y
+  local zones = (Server():getValue("duelzones") or "") 
+  s.pvpDamage = false
+  s:setValue("duelzone", false)
+  DuelZoneSector.isEnabled = false
+  print(zone..": PvP disabled")
+  Server():setValue("duelzones", string.gsub(stripZone(zones, zone),
+    "^:*(.-):*$", "%1"))
+end
+
+local function eternalon()
+  DuelZoneSector.isEternal = true
+end
+
+local function eternaloff()
+  DuelZoneSector.isEternal = false
+end
+
 
 -- DuelZoneSector.initialize sets pvp to 0 when this script is first
 --  loaded.
@@ -47,11 +77,10 @@ end
 --  None
 function DuelZoneSector.initialize()
   local s = Sector()
-  DuelZoneSector:DisablePVP()
-  s:setValue("duelzone", false)
   s:registerCallback("onPlayerLeft", "onPlayerLeft")
   s:registerCallback("onPlayerEntered", "onPlayerEntered")
 end
+
 
 -- DuelZoneSector.secure stores a table value containing the current
 --  state of this sectors namespace (DuelZoneSector)
@@ -60,38 +89,49 @@ end
 --  @0    Table
 function DuelZoneSector.secure()
   return {
-    eternal = (isEternal and 1 or 0),
-    enabled = (Sector().pvpDamage and 1 or 0)}
+    eternal = (DuelZoneSector.isEternal and 1 or 0),
+    enabled = (DuelZoneSector.isEnabled and 1 or 0)}
 end
 
 
 -- DuelZoneSector.restore restores the table returned in DuelZoneSector.secure
 --  on script reload. In practice, this is really only for making sure
---  that we are properly setting isEternal and the pvpState.
+--  that we are properly setting DuelZoneSector.isEternal and the pvpState.
 --
 -- Returns:
 --  None
 function DuelZoneSector.restore(data)
+  local s    = Sector()
+  local x, y = s:getCoordinates()
+  local name = x.."_"..y
+
   if type(data) == "nil" then
-    return DuelZoneSector.MakeEphemeral()
+    eternaloff()
+    pvpoff()
+    return
   end
 
   local eternalType = type(data["eternal"])
   local enabledType = type(data["enabled"])
 
-  if eternalType ~= "number" then
-    DuelZoneSector.MakeEphemeral()
+  if enabledType ~= "number" then
+    pvpoff()
   else
-    if data.eternal > 0 then
-      DuelZoneSector.MakeEternal()
+    if data.enabled > 0 then
+      pvpon()
+    else
+      pvpoff()
     end
   end
 
-  if enabledType ~= "number" then
-    DuelZoneSector.DisablePVP()
+
+  if eternalType ~= "number" and DuelZoneSector.isEnabled then
+    eternaloff()
   else
-    if data.enabled > 0 then
-      DuelZoneSector.EnablePVP()
+    if data.eternal > 0 then
+      eternalon()
+    else
+      eternaloff()
     end
   end
 end
@@ -106,8 +146,6 @@ end
 function DuelZoneSector.onRemove()
   local s = Sector()
 
-  DuelZoneSector.DisablePVP()
-
   if s:callbacksRegistered("onPlayerLeft", "onPlayerLeft") > 0 then
     s:unregisterCallback("onPlayerLeft", "onPlayerLeft")
   end
@@ -115,17 +153,19 @@ function DuelZoneSector.onRemove()
   if s:callbacksRegistered("onPlayerEntered", "onPlayerEntered") then
     s:unregisterCallback("onPlayerEntered", "onPlayerEntered")
   end
+
+  pvpoff()
 end
 
 
 -- DuelZoneSector.MakeEternal sets the zone to be a have pvp enabled
---  permanently when its activated. When isEternal is true, the 
+--  permanently when its activated. When DuelZoneSector.isEternal is true, the 
 --  onPlayerEntered and onPlayerLeft callbacks do nothing.
 --
 -- Returns:
 --  None
 function DuelZoneSector.MakeEternal()
-  isEternal = true
+  eternaloff()
 end
 
 
@@ -135,7 +175,7 @@ end
 -- Returns:
 --  None
 function DuelZoneSector.MakeEphemeral()
-  isEternal = false
+  eternaloff()
 end
 
 
@@ -144,24 +184,19 @@ end
 --
 -- Returns:
 --  None
-function DuelZoneSector.EnablePVP(is_eternal)
-  local s = Sector()
+function DuelZoneSector.EnablePVP(is_eternal, no_broadcast)
+  local s   = Sector()
   local msg = "This sector has been marked as a duelzone. PVP damage is enabled!"
 
-  if not s.pvpDamage then
-    s.pvpDamage = true
+  if not no_broadcast then
     s:broadcastChatMessage("", 3, msg)
-    isEternal = (is_eternal or false)
-    s:setValue("duelzone", true)
-
-    local x, y   = s:getCoordinates()
-    local zone   = x.."_"..y
-    local zones  = (Server():getValue("duelzones") or "")
-
-    Server():setValue("duelzones",
-      string.gsub(stripZone(zones, zone) .. ":" .. zone,
-      "^:*(.-):*$", "%1"))
   end
+
+  if is_eternal then
+    eternalon()
+  end
+
+  pvpon()
 end
 
 
@@ -170,27 +205,13 @@ end
 --
 -- Returns:
 --  None
-function DuelZoneSector.DisablePVP(msg)
-  local s = Sector()
+function DuelZoneSector.DisablePVP(msg, no_broadcast)
+  eternaloff()
+  pvpoff()
   
-  if type(msg) ~= "string" then
-    msg = "The fight has ended"
-  end
-
-  msg = "${m}. PVP is now disabled."%_T % {m = msg}
-
-  if s.pvpDamage then
-    s.pvpDamage = false
-    s:broadcastChatMessage("", 3, msg)
-    s:addScriptOnce("data/scripts/sector/background/warzonecheck.lua")
-    s:setValue("duelzone", false)
-
-    local x, y  = s:getCoordinates()
-    local zone  = x.."_"..y
-    local zones = (Server():getValue("duelzones") or "")
-
-    Server():setValue("duelzones", string.gsub(stripZone(zones, zone),
-      "^:*(.-):*$", "%1"))
+  if not no_broadcast then
+    Sector():broadcastChatMessage("", 2, (
+      msg and msg..", PVP is disabled." or "PVP is disabled."))
   end
 end
 
@@ -202,7 +223,7 @@ end
 --  None
 function DuelZoneSector.onPlayerLeft(index)
   local msg = "${p} has left"%_T % {p = Player(index).name}
-  if not isEternal then
+  if not DuelZoneSector.isEternal then
     DuelZoneSector.DisablePVP(msg)
   end
 end
@@ -216,15 +237,27 @@ end
 --  None
 function DuelZoneSector.onPlayerEntered(index)
   local player = Player(index)
+  local s = Sector()
+
   local msg = "${p} has arrived, and has interrupted the fight"%_T % {
     p = player.name}
 
-    if not isEternal then
-      DuelZoneSector.DisablePVP(msg)
-    else
-      if Sector().pvpDamage then
-        msg = "You have entered a designated PVP area."
-        player:sendChatMessage("", 2, msg)
+  -- Nested so that we don't run a len everytime this callback is run
+  if not DuelZoneSector.isEternal then
+    if DuelZoneSector.isEnabled then
+      if len(#{s:getPlayers()}) > 0 then
+        DuelZoneSector.DisablePVP(msg)
+        return
       end
     end
+  end
+
+  if  DuelZoneSector.isEternal
+  and DuelZoneSector.isEnabled then
+    msg = "You have entered a designated PVP area."
+    player:sendChatMessage("", 2, msg)
+    return
+  end
+
+  pvpoff()
 end
