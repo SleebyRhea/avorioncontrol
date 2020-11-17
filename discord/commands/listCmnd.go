37436dfd83f04461bfddc0e0b35d0036f6706539
Andrew Wyatt <sleepyfugu@gmail.com>
2020-11-17 00:55:52 -0500

    Only output commands the user has access to

--- discord/commands/listCmnd.go
+++ discord/commands/listCmnd.go
@@ -9,9 +9,19 @@ import (
 func listCmd(s *discordgo.Session, m *discordgo.MessageCreate, a BotArgs,
 	c ifaces.IConfigurator, cmd *CommandRegistrant) (*CommandOutput, ICommandError) {
 	var (
-		out = newCommandOutput(cmd, "Command Listing")
-		reg = cmd.Registrar()
+		out     = newCommandOutput(cmd, "Command Listing")
+		reg     = cmd.Registrar()
+		authlvl = 0
+		cnt     = 0
 	)
+
+	// Get the users authorization level
+	member, _ := s.GuildMember(reg.GuildID, m.Author.ID)
+	for _, r := range member.Roles {
+		if l := c.GetRoleAuth(r); l > authlvl {
+			authlvl = l
+		}
+	}
 
 	// Setting the description manually here, as helpCmd references this function
 	// directly when there are no commands provided.
@@ -19,19 +29,22 @@ func listCmd(s *discordgo.Session, m *discordgo.MessageCreate, a BotArgs,
 	out.Header = "Available Commands"
 	out.Quoted = true
 
-	cnt, cmnds := reg.AllCommands()
+	_, cmnds := reg.AllCommands()
+
+	for _, n := range cmnds {
+		cmd, _ := reg.Command(n)
+		authreq := c.GetCmndAuth(cmd.Name())
+		if c.CommandDisabled(cmd.Name()) || authlvl < authreq {
+			continue
+		}
+		out.AddLine(sprintf("**_%s_** - _%s_", cmd.Name(), cmd.description))
+		cnt++
+	}
+
 	if cnt < 1 {
 		out.AddLine("No commands available")
 		out.Construct()
 		return out, nil
-	}
-
-	for _, n := range cmnds {
-		cmd, _ := reg.Command(n)
-		if c.CommandDisabled(cmd.Name()) {
-			continue
-		}
-		out.AddLine(sprintf("**_%s_** - _%s_", cmd.Name(), cmd.description))
 	}
 
 	out.Construct()
