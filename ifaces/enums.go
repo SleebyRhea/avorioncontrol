e8962d9aaadc54164a6066a5bc3f26b1dd699a14
Andrew Wyatt <sleepyfugu@gmail.com>
2020-12-05 19:28:31 -0500

    Add State enums to ifaces

--- ifaces/enums.go
+++ ifaces/enums.go
@@ -13,6 +13,28 @@ const (
 	ServerCrashedStopping   = 258
 	ServerCrashedRestarting = 259
 
+	ServerOfflineColor           = 16711680
+	ServerOnlineColor            = 6749952
+	ServerStartingColor          = 3381759
+	ServerStoppingColor          = 16776960
+	ServerRestartingColor        = 3381759
+	ServerCrashedOfflineColor    = 15158332
+	ServerCrashedRecoveredColor  = 15158332
+	ServerCrashedStartingColor   = 15158332
+	ServerCrashedStoppingColor   = 15158332
+	ServerCrashedRestartingColor = 15158332
+
+	ServerOfflineString           = "Offline"
+	ServerOnlineString            = "Online"
+	ServerStartingString          = "Initializing"
+	ServerStoppingString          = "Stopping"
+	ServerRestartingString        = "Re-Initializing"
+	ServerCrashedOfflineString    = "Crashed (Dead)"
+	ServerCrashedRecoveredString  = "Crashed (Recovered)"
+	ServerCrashedStartingString   = "Crashed (Recovering)"
+	ServerCrashedStoppingString   = "Crashed (Attempting Graceful Exit)"
+	ServerCrashedRestartingString = "Crashed (Restarting)"
+
 	CommandSuccess = 0
 	CommandFailure = 1
 	CommandWarning = 2
@@ -35,6 +57,8 @@ const (
 )
 
 var difficultyMap map[int]string
+var stateMapString map[int]string
+var stateMapColor map[int]int
 
 func init() {
 	difficultyMap = make(map[int]string, 0)
@@ -45,6 +69,30 @@ func init() {
 	difficultyMap[difficultyExpert] = difficultyExpertString
 	difficultyMap[difficultyHardcore] = difficultyHardcoreString
 	difficultyMap[difficultyInsane] = difficultyInsaneString
+
+	stateMapString = make(map[int]string, 0)
+	stateMapString[ServerOffline] = ServerOfflineString
+	stateMapString[ServerOnline] = ServerOnlineString
+	stateMapString[ServerStarting] = ServerStartingString
+	stateMapString[ServerStopping] = ServerStoppingString
+	stateMapString[ServerRestarting] = ServerRestartingString
+	stateMapString[ServerCrashedOffline] = ServerCrashedOfflineString
+	stateMapString[ServerCrashedRecovered] = ServerCrashedRecoveredString
+	stateMapString[ServerCrashedStarting] = ServerCrashedStartingString
+	stateMapString[ServerCrashedStopping] = ServerCrashedStoppingString
+	stateMapString[ServerCrashedRestarting] = ServerCrashedRestartingString
+
+	stateMapColor = make(map[int]int, 0)
+	stateMapColor[ServerOffline] = ServerOfflineColor
+	stateMapColor[ServerOnline] = ServerOnlineColor
+	stateMapColor[ServerStarting] = ServerStartingColor
+	stateMapColor[ServerStopping] = ServerStoppingColor
+	stateMapColor[ServerRestarting] = ServerRestartingColor
+	stateMapColor[ServerCrashedOffline] = ServerCrashedOfflineColor
+	stateMapColor[ServerCrashedRecovered] = ServerCrashedRecoveredColor
+	stateMapColor[ServerCrashedStarting] = ServerCrashedStartingColor
+	stateMapColor[ServerCrashedStopping] = ServerCrashedStoppingColor
+	stateMapColor[ServerCrashedRestarting] = ServerCrashedRestartingColor
 }
 
 // Difficulty returns the difficulty string for a given int
@@ -58,4 +106,24 @@ func Difficulty(d int) string {
 	}
 
 	return difficultyMap[d]
+}
+
+// State returns the matching state string and color for a given server state
+func State(s int) (string, int) {
+	if s > ServerRestarting && s < ServerCrashedOffline {
+		s = ServerRestarting
+		return stateMapString[s], stateMapColor[s]
+	}
+
+	if s > ServerCrashedRestarting {
+		s = ServerCrashedRestarting
+		return stateMapString[s], stateMapColor[s]
+	}
+
+	if s < ServerOffline {
+		s = ServerOffline
+		return stateMapString[s], stateMapColor[s]
+	}
+
+	return stateMapString[s], stateMapColor[s]
 }
