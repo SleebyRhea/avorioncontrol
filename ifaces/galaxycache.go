a8a119aa1ef0f3ecb17653a5c7d81a9cdb2bd574
Andrew Wyatt <sleepyfugu@gmail.com>
2021-03-06 01:25:04 -0500

    Add Players to IGalaxyCache

--- ifaces/galaxycache.go
+++ ifaces/galaxycache.go
@@ -2,6 +2,7 @@ package ifaces
 
 // IGalaxyCache describes the fields required to be considered a cached Galaxy
 type IGalaxyCache interface {
+	Players() IPlayerCache
 	Name() string
 	Path() string
 }
