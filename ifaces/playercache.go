3da9d8da203175828106c4805a32b8e7399ca482
Andrew Wyatt <sleepyfugu@gmail.com>
2021-03-05 20:03:25 -0500

    Add player lookups to playercache interface

--- ifaces/playercache.go
+++ ifaces/playercache.go
@@ -2,4 +2,8 @@ package ifaces
 
 // IPlayerCache describes the fields required to be considered a player data cache
 type IPlayerCache interface {
+	FromName(string) IPlayer
+	FromFactionID(string) IPlayer
+	FromSteam64ID(string) IPlayer
+	FromDiscordID(string) IPlayer
 }
