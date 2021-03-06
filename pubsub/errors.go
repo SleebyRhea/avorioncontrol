81203e187da56ffa0f87c1882bec9a4dde49af05
Andrew Wyatt <sleepyfugu@gmail.com>
2021-03-06 02:22:14 -0500

    Add ErrMessageTimedOut

--- pubsub/errors.go
+++ pubsub/errors.go
@@ -1,6 +1,16 @@
+package pubsub
+
 type ErrUnexpectedMessageType struct {
 }
 
-func (e *ErrUnexpectedMessageType) {
+func (e *ErrUnexpectedMessageType) Error  {
 	
+}
+
+type ErrMessageTimedOut struct {
+	subid string
+}
+
+func (e *ErrMessageTimedOut) Error() string {
+	return "message failed to send due to a timeout on: "+e.subid
 }
