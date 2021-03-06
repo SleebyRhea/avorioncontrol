4e344f05263ae8f9a238067c825fbc38277da6c4
Andrew Wyatt <sleepyfugu@gmail.com>
2021-03-05 23:01:33 -0500

    Subscription is now MessageBus

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -5,9 +5,13 @@ import (
 	"sync"
 )
 
-// New returns a new Subscription
-func New(exit chan struct{}) *Subscription {
-	sub := &Subscription{
+// RCONBUSID is the string that describes the MessageBus subscription ID for
+// messaging the Avorion server
+const RCONBUSID = "ServerRCON"
+
+// New returns a new MessageBus
+func New(exit chan struct{}) *MessageBus {
+	sub := &MessageBus{
 		mutex: &sync.Mutex{},
 		in:    make(map[string]chan interface{}, 0),
 		out:   make(map[string][]chan interface{}, 0),
@@ -15,8 +19,8 @@ func New(exit chan struct{}) *Subscription {
 	return sub
 }
 
-// Subscription is a messaging bus interface to a generic type struct.
-type Subscription struct {
+// MessageBus is a messaging bus interface to a generic type struct.
+type MessageBus struct {
 	loglevel int
 	mutex    *sync.Mutex
 
@@ -24,34 +28,34 @@ type Subscription struct {
 	out map[string][]chan interface{}
 }
 
-// UUID returns the UUID of a pubsub.Subscription
-func (sub *Subscription) UUID() string {
+// UUID returns the UUID of a pubbus.Subscription
+func (bus *MessageBus) UUID() string {
 	return "MessagingBus"
 }
 
 // Loglevel returns the loglevel of an avorion.Server
-func (sub *Subscription) Loglevel() int {
-	return sub.loglevel
+func (bus *MessageBus) Loglevel() int {
+	return bus.loglevel
 }
 
 // SetLoglevel sets the loglevel of an avorion.Server
-func (sub *Subscription) SetLoglevel(l int) {
-	sub.loglevel = l
+func (bus *MessageBus) SetLoglevel(l int) {
+	bus.loglevel = l
 }
 
 // NewSubscription creates a new subscription and returns a channel with which
 // to publish on, and a function to cancel the subscription
-func (sub *Subscription) NewSubscription(name string) (chan interface{}, func()) {
+func (bus *MessageBus) NewSubscription(name string) (chan interface{}, func()) {
 	if name == "" {
-		logger.LogError(sub, "invalid subscription name provided (cannot be empty)")
+		logger.LogError(bus, "invalid subscription name provided (cannot be empty)")
 		return nil, nil
 	}
 
 	w := make(chan interface{}, 10)
-	if _, ok := sub.in[name]; ok {
-		w = sub.in[name]
+	if _, ok := bus.in[name]; ok {
+		w = bus.in[name]
 	} else {
-		sub.in[name] = w
+		bus.in[name] = w
 	}
 
 	cancel := func() {
@@ -61,37 +65,37 @@ func (sub *Subscription) NewSubscription(name string) (chan interface{}, func())
 	go func() {
 		for {
 			data := <-w
-			sub.mutex.Lock()
-			for _, out := range sub.out[name] {
+			bus.mutex.Lock()
+			for _, out := range bus.out[name] {
 				out <- data
 			}
-			sub.mutex.Unlock()
+			bus.mutex.Unlock()
 		}
 	}()
 
 	return w, cancel
 }
 
-// Subscribe provides an interface to listen for a message, and a function
+// Listen provides an interface to listen for a message, and a function
 // to cancel the subscription to the messaging bus.
-func (sub *Subscription) Subscribe(name string) (chan interface{}, func()) {
+func (bus *MessageBus) Listen(name string) (chan interface{}, func()) {
 	r := make(chan interface{}, 10)
-	if _, ok := sub.out[name]; !ok {
-		sub.out[name] = make([]chan interface{}, 0)
+	if _, ok := bus.out[name]; !ok {
+		bus.out[name] = make([]chan interface{}, 0)
 	}
 
-	sub.out[name] = append(sub.out[name], r)
+	bus.out[name] = append(bus.out[name], r)
 
 	cancel := func() {
-		sub.mutex.Lock()
-		defer sub.mutex.Unlock()
+		bus.mutex.Lock()
+		defer bus.mutex.Unlock()
 
-		if _, ok := sub.out[name]; ok {
-			for i, ch := range sub.out[name] {
+		if _, ok := bus.out[name]; ok {
+			for i, ch := range bus.out[name] {
 				if ch == r {
-					sub.out[name][i] = sub.out[name][len(sub.out[name])-1]
-					sub.out[name][len(sub.out[name])] = nil
-					sub.out[name] = sub.out[name][:len(sub.out[name])-1]
+					bus.out[name][i] = bus.out[name][len(bus.out[name])-1]
+					bus.out[name][len(bus.out[name])] = nil
+					bus.out[name] = bus.out[name][:len(bus.out[name])-1]
 					break
 				}
 			}
