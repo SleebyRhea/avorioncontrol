e267fb35fe091ae6b2b1ecd026b69693372ea68a
Andrew Wyatt <sleepyfugu@gmail.com>
2021-03-06 01:24:48 -0500

    Add NewPlayer to IPlayerCache

--- ifaces/playercache.go
+++ ifaces/playercache.go
@@ -2,6 +2,8 @@ package ifaces
 
 // IPlayerCache describes the fields required to be considered a player data cache
 type IPlayerCache interface {
+	NewPlayer(string, string, string, string) (IPlayer, error)
+
 	FromName(string) IPlayer
 	FromFactionID(string) IPlayer
 	FromSteam64ID(string) IPlayer
